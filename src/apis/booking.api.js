a7ca2d7051c9ef34c74c616ad693e7a7ebd1ff22
Earth <earthlono123@gmail.com>
2023-07-01 21:21:07 +1200

    remove ususe code

--- src/apis/booking.api.js
+++ src/apis/booking.api.js
@@ -2,6 +2,5 @@ import { apiInstance } from "configs/axios.config";
 
 export const addBookingAPI = (data) => apiInstance.post("/booking/add", data);
 export const getBookingAPI = () => apiInstance.get("/booking/get");
-export const getAllBookingAPI = () => apiInstance.get("/booking/getAll");
 export const getBookingByIdAPI = (orderId) => apiInstance.get(`/booking/getById/${orderId}`);
 
