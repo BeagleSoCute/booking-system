f6e650832da519700d94a3f63718a2f9cd78b592
Earth <earthlono123@gmail.com>
2023-07-07 10:35:19 +1200

    no message

--- src/smart/Login.jsx
+++ src/smart/Login.jsx
@@ -4,7 +4,7 @@ import { login } from "services/auth.service";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  console.log('Welcome to login page final');
+  console.log('Welcome to login page final test');
   const navigate = useNavigate();
   const handleOnFinish = async (values) => {
     const isLoginSuccess = await login(values);
