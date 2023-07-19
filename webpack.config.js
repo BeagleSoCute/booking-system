774abe4cdc6ac226ea1faf0dc4a068db95c69db6
BNZL\tanawat.lims <earthlono123@gmail.com>
2023-07-19 15:03:42 +1200

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   // Development-specific configurations
   devServer: {
     allowedHosts: [
+      "booking-frontend-vm.australiasoutheast.cloudapp.azure.com",
       "http://booking-frontend-vm.australiasoutheast.cloudapp.azure.com/",
     ],
   },
