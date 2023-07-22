ec10329ef17ebd23ed8aa745b57f77939f803d95
Earth <earthlono123@gmail.com>
2023-07-22 22:31:25 +1200

    Update path to point at the port 5000 instead

--- src/configs/axios.config.js
+++ src/configs/axios.config.js
@@ -6,7 +6,7 @@ import {
 import { notification } from "helpers/notification.helper";
 
 const apiInstance = axios.create({
-  baseURL:  "https://booking-backend-vscode.azurewebsites.net/api",
+  baseURL:  "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
