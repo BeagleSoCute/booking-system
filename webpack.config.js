708dfe7327ccf5c48780f01222c889fe32c782b9
Earth <earthlono123@gmail.com>
2023-07-28 14:24:05 +1200

    Add azure path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ module.exports = {
     allowedHosts: [
       "booking-google-cloud-platform.im-aim-thaicuisine.co.nz",
       ".im-aim-thaicuisine.co.nz",
-      "im-aim-thaicuisine.co.nz"
+      "im-aim-thaicuisine.co.nz", 
+      "booking-azure.australiaeast.cloudapp.azure.com",
+      ".australiaeast.cloudapp.azure.com",
+      "australiaeast.cloudapp.azure.com"
     ],
   },
 };
